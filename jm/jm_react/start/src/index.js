d2a665ea9f6a70b67eb2c0779dcd0eb5b2a9674a
DESKTOP-GR1OQL4\kjm <jinyinshu@nate.com>
2021-04-15 13:15:25 +0900

    message

--- jm/jm_react/start/src/index.js
+++ jm/jm_react/start/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 
 ReactDOM.render(<h1>Hello World</h1>, document.querySelector('#root'));
 
 
+=======
+>>>>>>> 1628c59c7f03b425b5ae212a4da8bdc046a446c2
 
